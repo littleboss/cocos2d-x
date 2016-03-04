bd155f7b002f05ff38e82045011f211014c2f38c
liaodingbai <liaodingbai@qq.com>
2016-03-04 21:13:36 +0800

    android编译

--- cocos/audio/android/jni/cddandroidAndroidJavaEngine.cpp
+++ cocos/audio/android/jni/cddandroidAndroidJavaEngine.cpp
@@ -26,6 +26,7 @@ THE SOFTWARE.
 #include "cddandroidAndroidJavaEngine.h"
 #include <stdlib.h>
 #include <android/log.h>
+#include <dlfcn.h>
 #include <jni.h>
 #include <sys/system_properties.h>
 #include "platform/android/jni/JniHelper.h"
@@ -41,6 +42,29 @@ THE SOFTWARE.
 
 using namespace cocos2d::experimental;
 using namespace CocosDenshion::android;
+
+#if (__ANDROID_API__ >= 21)
+// Android 'L' makes __system_property_get a non-global symbol.
+// Here we provide a stub which loads the symbol from libc via dlsym.
+typedef int (*PFN_SYSTEM_PROP_GET)(const char *, char *);
+int __system_property_get(const char* name, char* value)
+{
+    static PFN_SYSTEM_PROP_GET __real_system_property_get = NULL;
+    if (!__real_system_property_get) {
+        // libc.so should already be open, get a handle to it.
+        void *handle = dlopen("libc.so", RTLD_NOLOAD);
+        if (!handle) {
+            __android_log_print(ANDROID_LOG_ERROR, "foobar", "Cannot dlopen libc.so: %s.\n", dlerror());
+        } else {
+            __real_system_property_get = (PFN_SYSTEM_PROP_GET)dlsym(handle, "__system_property_get");
+        }
+        if (!__real_system_property_get) {
+            __android_log_print(ANDROID_LOG_ERROR, "foobar", "Cannot resolve __system_property_get(): %s.\n", dlerror());
+        }
+    }
+    return (*__real_system_property_get)(name, value);
+} 
+#endif // __ANDROID_API__ >= 21
 
 static inline bool getJNIStaticMethodInfo(cocos2d::JniMethodInfo &methodinfo,
     const char *methodName,
