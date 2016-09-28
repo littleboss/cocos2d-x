0f872e6b179d7cff46615a9d547d65cac3e0f58f
dangge <dangge@dangge.com>
2016-09-28 09:37:14 +0800

    android mk

--- cocos/platform/android/CCFileUtils-android.cpp
+++ cocos/platform/android/CCFileUtils-android.cpp
@@ -35,6 +35,7 @@ THE SOFTWARE.
 #include "base/ZipUtils.h"
 #include <stdlib.h>
 #include <sys/stat.h>
+#include "scripting/lua-bindings/manual/CCLuaEngine.h"
 
 #define  LOG_TAG    "CCFileUtils-android.cpp"
 #define  LOGD(...)  __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)
