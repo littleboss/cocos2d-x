c4f160c25ce3eabbcb542a7d854340e1deb29efc
zhengshouren <zhengshouren@qq.com>
2018-01-09 15:02:37 +0800

    xxtea_key不储存，展示的key是假的，混淆用

--- cocos/scripting/lua-bindings/manual/CCLuaStack.cpp
+++ cocos/scripting/lua-bindings/manual/CCLuaStack.cpp
@@ -110,6 +110,10 @@ LuaStack::~LuaStack()
     {
         lua_close(_state);
     }
+    if (nullptr != _mcode)
+    {
+        delete _mcode;
+    }
 }
 
 LuaStack *LuaStack::create(void)
@@ -755,12 +759,25 @@ void LuaStack::cleanupXXTEAKeyAndSign()
     }
 }
 
+void LuaStack::setMCodeKey(const char *key, int keyLen, const char *sign, int signLen){
+    _mKey = (char*)malloc(keyLen);
+    memcpy(_mKey, key, keyLen);
+    _mKeyLen = keyLen;
+    
+    _mSign = (char*)malloc(signLen);
+    memcpy(_mSign, sign, signLen);
+    _mSignLen = signLen;
+    _mcode = new MCODE((unsigned char*)_mSign);
+}
+
 unsigned char* LuaStack::xxteaDecrypt(unsigned char *buffer, ssize_t size, ssize_t *outlen)
 {
     xxtea_long len = 0;
+    char key[128];
+    _mcode->encode(_mKey, key);
     unsigned char *outbuffer = xxtea_decrypt(buffer + _xxteaSignLen,
                                              (xxtea_long)size - (xxtea_long)_xxteaSignLen,
-                                             (unsigned char*)_xxteaKey,
+                                             (unsigned char*)key,
                                              (xxtea_long)_xxteaKeyLen,
                                              &len);
     *outlen = len;
@@ -808,26 +825,26 @@ int LuaStack::luaLoadChunksFromZIP(lua_State *L)
         unsigned char* bytes = zipFileData.getBytes();
         ssize_t size = zipFileData.getSize();
 
-        bool isXXTEA = stack && stack->_xxteaEnabled && size >= stack->_xxteaSignLen
-            && memcmp(stack->_xxteaSign, bytes, stack->_xxteaSignLen) == 0;
-
-
-        if (isXXTEA) { // decrypt XXTEA
+        if (isXXTEA(bytes, size)) { // decrypt XXTEA
             xxtea_long len = 0;
+            char key[128];
+            stack->_mcode->encode(stack->_mKey, key);
             buffer = xxtea_decrypt(bytes + stack->_xxteaSignLen,
                                    (xxtea_long)size - (xxtea_long)stack->_xxteaSignLen,
-                                   (unsigned char*)stack->_xxteaKey,
+                                   (unsigned char*)key,
                                    (xxtea_long)stack->_xxteaKeyLen,
                                    &len);
             zip = ZipFile::createWithBuffer(buffer, len);
         } else {
+#if CC_TARGET_PLATFORM == CC_PLATFORM_WIN32 || CC_TARGET_PLATFORM == CC_PLATFORM_MAC
             if (size > 0) {
                 zip = ZipFile::createWithBuffer(bytes, (unsigned long)size);
             }
+#endif
         }
 
         if (zip) {
-            CCLOG("lua_loadChunksFromZIP() - load zip file: %s%s", zipFilePath.c_str(), isXXTEA ? "*" : "");
+            CCLOG("lua_loadChunksFromZIP() - load zip file: %s", zipFilePath.c_str());
             lua_getglobal(L, "package");
             lua_getfield(L, -1, "preload");
 
@@ -900,13 +917,15 @@ int LuaStack::luaLoadBuffer(lua_State *L, const char *chunk, int chunkSize, cons
 {
     int r = 0;
 
-    if (_xxteaEnabled && strncmp(chunk, _xxteaSign, _xxteaSignLen) == 0)
+    if (isXXTEA((unsigned char*)chunk, chunkSize))
     {
         // decrypt XXTEA
         xxtea_long len = 0;
+        char key[128];
+        _mcode->encode(_mKey, key);
         unsigned char* result = xxtea_decrypt((unsigned char*)chunk + _xxteaSignLen,
                                               (xxtea_long)chunkSize - _xxteaSignLen,
-                                              (unsigned char*)_xxteaKey,
+                                              (unsigned char*)key,
                                               (xxtea_long)_xxteaKeyLen,
                                               &len);
         skipBOM((const char*&)result, (int&)len);
@@ -915,8 +934,10 @@ int LuaStack::luaLoadBuffer(lua_State *L, const char *chunk, int chunkSize, cons
     }
     else
     {
-        skipBOM(chunk, chunkSize);
-        r = luaL_loadbuffer(L, chunk, chunkSize, chunkName);
+#if CC_TARGET_PLATFORM == CC_PLATFORM_WIN32 || CC_TARGET_PLATFORM == CC_PLATFORM_MAC
+       skipBOM(chunk, chunkSize);
+       r = luaL_loadbuffer(L, chunk, chunkSize, chunkName);
+#endif
     }
 
 #if defined(COCOS2D_DEBUG) && COCOS2D_DEBUG > 0
