c75aeb5aa62304ffe557c00c1ac52e1e522a3114
zhengshouren <448949750@qq.com>
2018-11-06 17:15:53 +0800

    修改字体

--- cocos/2d/CCLabel.cpp
+++ cocos/2d/CCLabel.cpp
@@ -474,7 +474,7 @@ void Label::reset()
     _outlineSize = 0.f;
     _bmFontPath = "";
     _systemFontDirty = false;
-    _systemFont = "font/GenJyuuGothic-P-Medium.ttf";
+    _systemFont = "font/SourceHanSansTW-Medium.ttf";
     _systemFontSize = 12;
 
     if (_horizontalKernings)
